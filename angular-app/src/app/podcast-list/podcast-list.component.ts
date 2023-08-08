87485ab48702b9a3a08555df72193a02ac0afd4e
Adrià <adria.huertas@aetech.biz>
2023-08-08 10:31:04 +0200

    Canvis menors

--- angular-app/src/app/podcast-list/podcast-list.component.ts
+++ angular-app/src/app/podcast-list/podcast-list.component.ts
@@ -10,11 +10,15 @@ export class PodcastListComponent implements OnInit {
     'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
   private podcastData: Array<{}> = [];
 
+  // Keys to use in localStorage
+  private fetchingDateKey: string = 'fetchingDate';
+  private podcastDataKey: string = 'podcastData';
+
   // Function to check if 24h has passed from previous fetching
-  has24HoursPassed(): boolean {
+  have24HoursPassed(): boolean {
     const millisecondsIn24Hours = 24 * 60 * 60 * 1000;
     // Retrieve previous fetching date
-    const previousStoredData = localStorage.getItem('fetchingDate');
+    const previousStoredData = localStorage.getItem(this.fetchingDateKey);
     if (previousStoredData !== null) {
       const currentDate = new Date();
       const previousDate = new Date(previousStoredData);
@@ -30,23 +34,27 @@ export class PodcastListComponent implements OnInit {
   // When mounting the component
   ngOnInit(): void {
     // Check if it's been 24h from previous fetching
-    if (this.has24HoursPassed()) {
+    if (this.have24HoursPassed()) {
       console.log('Fetching from External API');
+
       // Fetch data from API
       fetch(this.url)
         .then((response) => response.json())
         .then((data) => {
           // Save data to localStorage
-          localStorage.setItem('podcastData', JSON.stringify(data.feed.entry));
+          localStorage.setItem(
+            this.podcastDataKey,
+            JSON.stringify(data.feed.entry)
+          );
           // Update fetching date
-          localStorage.setItem('fetchingDate', new Date().toString());
+          localStorage.setItem(this.fetchingDateKey, new Date().toString());
           return (this.podcastData = data);
         });
     } else {
-      // Fetching from localStorage
+      // "Fetching" from localStorage
       console.log('Fetching from localStorage');
       this.podcastData = JSON.parse(
-        localStorage.getItem('podcastData') as string
+        localStorage.getItem(this.podcastDataKey) as string
       );
     }
   }
